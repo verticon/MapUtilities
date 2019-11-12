3893ab25d4bddb9976f3eb367342f3844c5dde7f
Robert Vaessen <robert@rvaessen.com>
2019-11-12 10:42:21 -0500

    Reorganizing

---
+++ OverviewDetail/OverviewDetail/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  OverviewDetail
+//
+//  Created by Robert Vaessen on 11/12/19.
+//  Copyright © 2019 Robert Vaessen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+}
+
